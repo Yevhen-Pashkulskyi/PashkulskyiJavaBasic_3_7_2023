6397d2b459496d533894e7a04571f5964fa15b13
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-06 10:30:36 +0300

    add second commit

--- src/hillel/homework/lesson5/GitComit.java
+++ src/hillel/homework/lesson5/GitComit.java
@@ -3,5 +3,6 @@ package hillel.homework.lesson5;
 public class GitComit {
     public static void main(String[] args) {
         System.out.println("my first commit");
+        System.out.println("second commit");
     }
 }
