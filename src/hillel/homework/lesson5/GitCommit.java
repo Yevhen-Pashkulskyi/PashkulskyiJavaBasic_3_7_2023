31b5b28ddd4ac43986b689f3b252fd4fcbd02667
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-06 10:31:30 +0300

    rename class

--- src/hillel/homework/lesson5/GitCommit.java
+++ src/hillel/homework/lesson5/GitCommit.java
@@ -1,6 +1,6 @@
 package hillel.homework.lesson5;
 
-public class GitComit {
+public class GitCommit {
     public static void main(String[] args) {
         System.out.println("my first commit");
         System.out.println("second commit");
