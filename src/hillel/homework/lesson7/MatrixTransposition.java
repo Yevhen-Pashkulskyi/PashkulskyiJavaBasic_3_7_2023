327803e27ce7086d8dbf3ef18772f73d9e92e6fe
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-11 23:26:55 +0300

    lesson7 matrix print corrected 2

--- src/hillel/homework/lesson7/MatrixTransposition.java
+++ src/hillel/homework/lesson7/MatrixTransposition.java
@@ -69,10 +69,6 @@ public class MatrixTransposition {
 //    }
     private static void print(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i] == null) {
-                System.out.println();
-                continue;
-            }
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
