f19144e853686b0df5b57eafde5bd7e0d43a81a7
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-13 13:26:46 +0300

    lesson8 person info (version 2)

--- src/hillel/homework/lesson8/PersonInfo.java
+++ src/hillel/homework/lesson8/PersonInfo.java
@@ -7,28 +7,28 @@ public class PersonInfo {
     String phoneNumber;
 
     public static void main(String[] args) {
-        PersonInfo personWillSmith = new PersonInfo();
-        personWillSmith.firstName = " Will ";
-        personWillSmith.surname = " Smith ";
-        personWillSmith.city = " New York ";
-        personWillSmith.phoneNumber = " 2936729462846";
+        PersonInfo personWillSmith = new PersonInfo("Will", "Smith", "New York", "2936729462846");
+//        personWillSmith.firstName = " Will ";
+//        personWillSmith.surname = " Smith ";
+//        personWillSmith.city = " New York ";
+//        personWillSmith.phoneNumber = " 2936729462846";
 
         personWillSmith.personInfo();
 
-        PersonInfo personJackieChan = new PersonInfo();
-        personJackieChan.firstName = " Jackie ";
-        personJackieChan.surname = " Chan ";
-        personJackieChan.city = " Shanghai ";
-        personJackieChan.phoneNumber = " 12312412412";
+        PersonInfo personJackieChan = new PersonInfo("Jackie");
+//        personJackieChan.firstName = " Jackie ";
+//        personJackieChan.surname = " Chan ";
+//        personJackieChan.city = " Shanghai ";
+//        personJackieChan.phoneNumber = " 12312412412";
 
         personJackieChan.personInfo();
-
-        PersonInfo personSherlockHolmes = new PersonInfo();
-        personSherlockHolmes.firstName = " Sherlock ";
-        personSherlockHolmes.surname = " Holmes ";
-        personSherlockHolmes.city = " London ";
-        personSherlockHolmes.phoneNumber = " 37742123513";
-
+//
+        PersonInfo personSherlockHolmes = new PersonInfo("Sherlock", "Holmes");
+//        personSherlockHolmes.firstName = " Sherlock ";
+//        personSherlockHolmes.surname = " Holmes ";
+//        personSherlockHolmes.city = " London ";
+//        personSherlockHolmes.phoneNumber = " 37742123513";
+//
         personSherlockHolmes.personInfo();
 
     }
@@ -37,5 +37,24 @@ public class PersonInfo {
         System.out.println("Зателефонувати громадянинові" + firstName + surname + "з міста" + city + "можна за номером" + phoneNumber + ".");
     }
 
+    public PersonInfo(String will, String smith, String city, String phoneNumber) {
+        firstName = " Will ";
+        surname = " Smith ";
+        this.city = " New York ";
+        this.phoneNumber = " 2936729462846";
+    }
 
+    public PersonInfo(String jackie) {
+        firstName = " Jackie ";
+        surname = " Chan ";
+        city = " Shanghai ";
+        phoneNumber = " 12312412412";
+    }
+
+    public PersonInfo(String sherlock, String surname) {
+        firstName = " Sherlock ";
+        this.surname = " Holmes ";
+        city = " London ";
+        phoneNumber = " 37742123513";
+    }
 }
