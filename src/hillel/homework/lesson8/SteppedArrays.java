81a565613bcc020900031967ee78fb8964f6fd69
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-16 13:37:40 +0300

    lesson8 Stepped Arrays: add method sync sort elements

--- src/hillel/homework/lesson8/SteppedArrays.java
+++ src/hillel/homework/lesson8/SteppedArrays.java
@@ -16,19 +16,23 @@ public class SteppedArrays {
 //        lines = scanner.nextInt();
 //        maxCountElements = scanner.nextInt();
         int[][] dataArray = new int[lines][random(minRandom, maxRandom)];
-        int[][] result = array(dataArray);
+        int[][] array = array(dataArray);
         System.out.println("array");
-        print(result);
+        print(array);
 
         System.out.println("sort : ");
-        sortElements(result);// нужно будет убрать , єто лішній массів
-//        print(sort);
-        System.out.println("revers : ");
-        reversSortElement(result);// нужно будет убрать , єто лішній массів
+//        syncSortElements(array);// нужно будет убрать , єто лішній массів
+        print(syncSortElements(array));
+//        System.out.println("revers : ");
+//        reversSortElement(array);// нужно будет убрать , єто лішній массів
 //        print(revers);
+
+//        System.out.println("чередованіе четное і нечетное");
+//        print(syncSortReverse(array));
 
     }
 
+    // метод создает массів
     public static int[][] array(int[][] dataArray) {
 
         for (int i = 0; i < dataArray.length; i++) {
@@ -45,20 +49,30 @@ public class SteppedArrays {
         return dataArray;
     }
 
-    public static int[][] syncReverse(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array.length % 2 = 0) {
+    //
+//    public static int[][] syncSortReverse(int[][] array) {
+//       int [][] syncArray  ;
+//
+//        for (int i = 0; i < array.length; i++) {
+//
+//            if (i % 2 == 0) {
+//                i
+//            } else {
+//               array = reversSortElement(array);
+//            }
+//        }
+//        return syncArray;
+//    }
 
-            }
-        }
-        return array;
-    }
 
+    //метод создает рандомное чісло в діапозоне мін і макс
     public static int random(int minRandom, int maxRandom) {
         int random = ThreadLocalRandom.current().nextInt(minRandom, maxRandom);
         return random;
     }
 
+
+    //меттод виводіт массіви
     public static void print(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -68,45 +82,94 @@ public class SteppedArrays {
         }
     }
 
-    public static void /*int[][]*/ sortElements(int[][] array) {
-        boolean swapped;
-        do {
-            swapped = false;
-            for (int i = 0; i < array.length; i++) {
 
-                int steps = array[i].length - 1;
-
-                for (int j = 0; j < steps; j++) {
-                    if (array[i][j] > array[i][j + 1]) {
-                        int temp = array[i][j];
-                        array[i][j] = array[i][j + 1];
-                        array[i][j + 1] = temp;
-                        swapped = true;
+    public static int[][] syncSortElements(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                // Сортировка четной строки по возрастанию (сортировка выбором)
+                for (int j = 0; j < array[i].length - 1; j++) {
+                    int minIndex = j;
+                    for (int k = j + 1; k < array[i].length; k++) {
+                        if (array[i][k] < array[i][minIndex]) {
+                            minIndex = k;
+                        }
+                    }
+                    int temp = array[i][j];
+                    array[i][j] = array[i][minIndex];
+                    array[i][minIndex] = temp;
+                }
+            } else {
+                // Сортировка нечетной строки по убыванию (сортировка пузырьком)
+                for (int j = 0; j < array[i].length - 1; j++) {
+                    for (int k = 0; k < array[i].length - j - 1; k++) {
+                        if (array[i][k] < array[i][k + 1]) {
+                            int temp = array[i][k];
+                            array[i][k] = array[i][k + 1];
+                            array[i][k + 1] = temp;
+                        }
                     }
                 }
             }
-        } while (swapped);
-//        return array;
+        }
+        return array;
     }
 
-    public static void /*int[][]*/ reversSortElement(int[][] array) {
-        boolean swapped;
-        do {
-            swapped = false;
-            for (int i = 0; i < array.length; i++) {
+    private static int sumElements(int[][] array) {
+        int sum = 0;
+        for (; ; ) {
+            for (; ; ) {
 
-                int steps = array[i].length - 1;
-
-                for (int j = 0; j < steps; j++) {
-                    if (array[i][j] < array[i][j + 1]) {
-                        int temp = array[i][j];
-                        array[i][j] = array[i][j + 1];
-                        array[i][j + 1] = temp;
-                        swapped = true;
-                    }
-                }
             }
-        } while (swapped);
-//        return array;
+        }
+        return sum;
     }
+
+//    //метод сортирует строку по возростанию
+//    public static int[][] sortElements(int[][] array) {
+//        boolean swapped;
+//        do {
+//            swapped = false;
+//            for (int i = 0; i < array.length; i = i + 2) {
+//
+//                if (i % 2 == 1) {
+//                    continue;
+//                } else {
+//                    for (int j = 0; j < array[i].length - 1; j++) {
+//                        if (array[i][j] > array[i][j + 1]) {
+//                            int temp = array[i][j];
+//                            array[i][j] = array[i][j + 1];
+//                            array[i][j + 1] = temp;
+//                            swapped = true;
+//                        }
+//                    }
+//                }
+//            }
+//        } while (swapped);
+//        return array;
+//    }
+//
+//
+//    //метод сортирует по убиванию
+//    public static int[][] reversSortElement(int[][] array) {
+//        boolean swapped;
+//        do {
+//            swapped = false;
+//            for (int i = 0; i < array.length; i++) {
+//
+//                int steps = array[i].length - 1;
+//
+//                for (int j = 0; j < steps; j++) {
+//                    if (array[i][j] < array[i][j + 1]) {
+//                        int temp = array[i][j];
+//                        array[i][j] = array[i][j + 1];
+//                        array[i][j + 1] = temp;
+//                        swapped = true;
+//                    }
+//                }
+//            }
+//        } while (swapped);
+//        return array;
+//    }
+
+
 }
