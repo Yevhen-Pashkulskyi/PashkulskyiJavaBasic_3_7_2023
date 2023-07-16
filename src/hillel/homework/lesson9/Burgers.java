1f500cfbd00bd7284a5f8c4a9b3d3055761191d2
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-17 00:01:49 +0300

    lesson9 Burgers with BurgerMain ( version 1.2 )

--- src/hillel/homework/lesson9/Burgers.java
+++ src/hillel/homework/lesson9/Burgers.java
@@ -2,13 +2,25 @@ package hillel.homework.lesson9;
 
 public class Burgers {
     private String bun;
+    private int quantity;
     private String meat;
     private String cheese;
     private String greens;
     private String mayonnaise;
 
-    public void compositionBurger() {
+    public void classicBurger() {
+        System.out.println("Класичний бургер");
+        System.out.println("Склад: " + bun + ", " + meat + ", " + cheese + ", " + greens + ", " + mayonnaise);
+    }
 
+    public void dietBurger() {
+        System.out.println("Дієтичний бургер");
+        System.out.println("Склад: " + bun + ", " + meat + ", " + cheese + ", " + greens);
+    }
+
+    public void doubleBurger() {
+        System.out.println("Double бургер");
+        System.out.println("Склад: " + bun + ", " + quantity + "-" + meat + ", " + cheese + ", " + greens + ", " + mayonnaise);
     }
 
     public Burgers(String bun, String meat, String cheese, String greens, String mayonnaise) {
@@ -25,11 +37,26 @@ public class Burgers {
         this.cheese = cheese;
         this.greens = greens;
     }
+
+    public Burgers(String bun, int quantity, String meat, String cheese, String greens, String mayonnaise) {
+        this.bun = bun;
+        this.quantity = quantity;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayonnaise = mayonnaise;
+    }
 }
 
 class BurgerMain {
     public static void main(String[] args) {
-        Burgers burgers = new Burgers("bun", "meat", "cheese", "greens", "mayonnaise");
-        burgers.compositionBurger();
+        Burgers classicBurger = new Burgers("булочка", "м'ясо", "сир", "зелень", "майонез");
+        classicBurger.classicBurger();
+
+        Burgers dietBurger = new Burgers("булочка", "м'ясо", "сир", "зелень");
+        dietBurger.dietBurger();
+
+        Burgers doubleBurger = new Burgers("булочка", 2, "м'яса", "сир", "зелень", "майонез");
+        doubleBurger.doubleBurger();
     }
 }
