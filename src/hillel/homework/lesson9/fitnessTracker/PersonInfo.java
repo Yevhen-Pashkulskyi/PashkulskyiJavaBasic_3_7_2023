7cd41e5b8ebb3e39a9dd1b2474992906d1093a79
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-19 19:20:08 +0300

    lesson9 Fitness tracker (version 2)

--- src/hillel/homework/lesson9/fitnessTracker/PersonInfo.java
+++ src/hillel/homework/lesson9/fitnessTracker/PersonInfo.java
@@ -1,32 +1,54 @@
 package hillel.homework.lesson9.fitnessTracker;
 
 public class PersonInfo {
-    private final String name;
-    private final int dayBirthday;
-    private final int monthBirthday;
-    private final int yearBirthday;
+    private String name;
+    private int dayBirthday;
+    private int monthBirthday;
+    private int yearBirthday;
+    private String email;
+    private String phoneNumber;
+
     private int age;
+
     private String surname;
+
     private int weight;
     private int systolicPressure;
     private int diastolicPressure;
     private int step;
 
-
-    public PersonInfo(String name, String surname, int dayBirthday, int monthBirthday, int yearBirthday, int weight, int systolicPressure, int diastolicPressure, int step) {
+    private String getName(String name) {
         this.name = name;
-        this.surname = surname;
-        this.dayBirthday = dayBirthday;
-        this.monthBirthday = monthBirthday;
-        this.yearBirthday = yearBirthday;
-        this.weight = weight;
-        this.systolicPressure = systolicPressure;
-        this.diastolicPressure = diastolicPressure;
-        this.step = step;
+        return name;
     }
 
     void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    private int getDayBirthday(int dayBirthday) {
+        this.dayBirthday = dayBirthday;
+        return dayBirthday;
+    }
+
+    private int getMonthBirthday(int monthBirthday) {
+        this.monthBirthday = monthBirthday;
+        return monthBirthday;
+    }
+
+    private int getYearBirthday(int yearBirthday) {
+        this.yearBirthday = yearBirthday;
+        return yearBirthday;
+    }
+
+    private String getEmail(String email) {
+        this.email = email;
+        return email;
+    }
+
+    private String getPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return phoneNumber;
     }
 
     public void setWeight(int weight) {
@@ -45,20 +67,32 @@ public class PersonInfo {
         this.step = step;
     }
 
-    public int getYearBirthday() {
-        return yearBirthday;
-    }
-
     public int getAge(int yearToDay) {
-        age = yearToDay - getYearBirthday();
+        age = yearToDay - getYearBirthday(yearBirthday);
         return age;
     }
 
+    public PersonInfo(String name, String surname, int dayBirthday, int monthBirthday, int yearBirthday, String email, String phoneNumber, int weight, int systolicPressure, int diastolicPressure, int step) {
+        getName(name);
+        setSurname(surname);
+        getDayBirthday(dayBirthday);
+        getMonthBirthday(monthBirthday);
+        getYearBirthday(yearBirthday);
+        getEmail(email);
+        getPhoneNumber(phoneNumber);
+        setWeight(weight);
+        setSystolicPressurePressure(systolicPressure);
+        setDiastolicPressure(diastolicPressure);
+        setStep(step);
+    }
+
     public void printAccountInfo() {
-        System.out.println("Name: " + name);
+        System.out.println("Name: " + getName(name));
         System.out.println("Surname: " + surname);
-        System.out.println("Date of birth: " + dayBirthday + "." + monthBirthday + "." + yearBirthday);
+        System.out.println("Date of birth: " + dayBirthday + "." + monthBirthday + "." + getYearBirthday(yearBirthday));
         System.out.println("Age: " + age);
+        System.out.println("Email: " + getEmail(email));
+        System.out.println("Phone number: " + getPhoneNumber(phoneNumber));
         System.out.println("Weight: " + weight + " kg");
         System.out.println("Pressure: " + "s." + systolicPressure + " / " + "d." + diastolicPressure);
         System.out.println("Step: " + step);
