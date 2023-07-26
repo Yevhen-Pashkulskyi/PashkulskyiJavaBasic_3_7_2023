1a6df6130b6e311ec1faeded2ab10ec0fd91c2f5
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-26 15:44:33 +0300

    lesson11 Converter Temperature version 1.1

--- src/hillel/homework/lesson11/demonstration/DemonstrationConverter.java
+++ src/hillel/homework/lesson11/demonstration/DemonstrationConverter.java
@@ -11,19 +11,18 @@ public class DemonstrationConverter {
         Converter celsiusToKelvin = new CelsiusToKelvin();
         Converter celsiusToFahrenheit = new CelsiusToFahrenheit();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter temperature to celsius:");
+        System.out.println("Enter temperature to celsius, separated by comma :");
 
         double celsius = scanner.nextDouble();
 
         double fahrenheit = celsiusToFahrenheit.convertFromCelsius(celsius);
-        System.out.println("Celsius to fahrenheit result:\t" + fahrenheit);
+        System.out.printf("Celsius to fahrenheit result:\t %.2f %n", fahrenheit);
         double fahrenheitToCelsius = celsiusToFahrenheit.convertToCelsius(fahrenheit);
-        System.out.println("Fahrenheit to celsius result:\t" + fahrenheitToCelsius);
+        System.out.printf("Fahrenheit to celsius result:\t %.2f %n", fahrenheitToCelsius);
 
         double kelvin = celsiusToKelvin.convertFromCelsius(celsius);
-        System.out.println("Celsius to kelvin result:\t" + kelvin);
+        System.out.printf("Celsius to kelvin result:\t %.2f %n", kelvin);
         double kelvinToCelsius = celsiusToKelvin.convertToCelsius(kelvin);
-        System.out.println("Kelvin to celsius result:\t" + kelvinToCelsius);
-
+        System.out.printf("Kelvin to celsius result:\t %.2f %n", kelvinToCelsius);
     }
 }
