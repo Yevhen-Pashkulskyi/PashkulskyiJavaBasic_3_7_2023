f665d2f40f3adc8f028af9d1c3d6fec285e15949
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-07-31 15:14:45 +0300

    lesson 12 test for point

---
+++ src/test/java/hillel/homework/lesson10/point/PointTest.java
@@ -0,0 +1,48 @@
+package hillel.homework.lesson10.point;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class PointTest {
+    @Test
+    public void testDistanceTo() {
+        Point firestPoint = new Point(0, 0);
+        Point secondPoint = new Point(3, 4);
+        double expectedDistance = 5.0;
+        double actualDistance = firestPoint.distanceTo(secondPoint);
+        assertEquals(expectedDistance, actualDistance, 0.001); // Точність обчислень - 0.001
+    }
+
+    @Test
+    public void testDistanceBetween() {
+        Point firestPoint = new Point(0, 0);
+        Point secondPoint = new Point(3, 4);
+        double expectedDistance = 5.0;
+        double actualDistance = Point.distanceBetween(firestPoint, secondPoint);
+        assertEquals(expectedDistance, actualDistance, 0.001); // Точність обчислень - 0.001
+    }
+
+    @Test
+    public void testEquals() {
+        Point firestPoint = new Point(2, 3);
+        Point secondPoint = new Point(2, 3);
+        Point thirdPoint = new Point(4, 5);
+
+        assertEquals(firestPoint, secondPoint);
+        assertNotEquals(firestPoint, thirdPoint);
+    }
+
+    @Test
+    public void testHashCode() {
+        Point firestPoint = new Point(2, 3);
+        Point secondPoint = new Point(2, 3);
+        Point thirdPoint = new Point(4, 5);
+
+        assertEquals(firestPoint.hashCode(), secondPoint.hashCode());
+        assertNotEquals(firestPoint.hashCode(), thirdPoint.hashCode());
+    }
+}
+
+
