66ad93cfcd6e7f4785a3b71c63fc4bd330c68d51
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-08-06 13:31:04 +0300

    lesson14 User login password version 1.0.2

--- src/main/java/hillel/homework/lesson14/verificationLoginAndPassword/User.java
+++ src/main/java/hillel/homework/lesson14/verificationLoginAndPassword/User.java
@@ -7,33 +7,19 @@ import hillel.homework.lesson14.verificationLoginAndPassword.exception.WrongPass
 
 public class User {
     private static final int MAX_LOGIN_LENGTH = 20;
-
     private static final int MIN_PASSWORD_LENGTH = 6;
     private static final int MAX_PASSWORD_LENGTH = 25;
     private String login;
     private String password;
     private String confirmPassword;
 
-//    public User(String login, String password, String confirmPassword) {
-//        validationLogin(login);
-//        lengthValidationPassword(password);
-//        validationPassword(password);
-//        equalsValidationPassword(password, confirmPassword);
-//
-//        this.login = login;
-//        this.password = password;
-//        this.confirmPassword = confirmPassword;
-//    }
-
-    public User(String login) {
+    public User(String login, String password, String confirmPassword) {
         validationLogin(login);
-        this.login = login;
-    }
-
-    public User(String password, String confirmPassword) {
         lengthValidationPassword(password);
+        validationPassword(password);
         equalsValidationPassword(password, confirmPassword);
-        validationLogin(password);
+
+        this.login = login;
         this.password = password;
         this.confirmPassword = confirmPassword;
     }
@@ -45,7 +31,7 @@ public class User {
     }
 
     private void lengthValidationPassword(String password) {
-        if (MIN_PASSWORD_LENGTH > password.length() || password.length() > MAX_PASSWORD_LENGTH) {
+        if (!password.matches("^.{6,20}$")) { // (MIN_PASSWORD_LENGTH > password.length() || password.length() > MAX_PASSWORD_LENGTH){
             throw new WrongLengthPasswordException("The length of the password must be between 6 and 25 characters.");
         }
     }
@@ -55,12 +41,42 @@ public class User {
             throw new WrongPasswordException("The password must contain at least one letter, one number.");
         }
     }
+//    private void validationPassword(String password) {
+//        char[] symbolPassword = password.toCharArray();
+//        boolean letter = false;
+//        boolean digit = false;
+//
+////        for (char symbol : symbolPassword) {
+////            if (Character.isLetter(symbol)) {
+////                letter = true;
+////            } else if (Character.isDigit(symbol)) {
+////                digit = true;
+////            }
+////
+////            if (letter && digit) {
+////                return;
+////            }
+////        }
+//        for (int i = 0; i < symbolPassword.length; i++){
+//            if (Character.isLetter(symbolPassword[i])){
+//                letter =true;
+//            } else if (Character.isDigit(symbolPassword[i])) {
+//                digit = true;
+//            }
+//            if (letter && digit){
+//                return;
+//            }
+//        }
+//
+//        throw new WrongPasswordException("The password must contain at least one letter and one number.");
+//    }
 
     private void equalsValidationPassword(String password, String confirmPassword) {
-        if (!password.equals(confirmPassword) || password == null) {
+        if (!password.equals(confirmPassword)) {
             throw new WrongEqualsPasswordException("Password must match");
         }
     }
+
     public String getLogin() {
         return login;
     }
--- src/main/java/hillel/homework/lesson14/verificationLoginAndPassword/UserDemo.java
+++ src/main/java/hillel/homework/lesson14/verificationLoginAndPassword/UserDemo.java
@@ -2,6 +2,7 @@ package hillel.homework.lesson14.verificationLoginAndPassword;
 
 import hillel.homework.lesson14.verificationLoginAndPassword.exception.WrongEqualsPasswordException;
 import hillel.homework.lesson14.verificationLoginAndPassword.exception.WrongLengthPasswordException;
+import hillel.homework.lesson14.verificationLoginAndPassword.exception.WrongLoginException;
 import hillel.homework.lesson14.verificationLoginAndPassword.exception.WrongPasswordException;
 
 import java.util.Scanner;
@@ -14,65 +15,26 @@ public class UserDemo {
         int attemptCounter = 0;
 
         while (attemptCounter < COUNTER) {
-//            try {
-            while (attemptCounter < COUNTER) {
-                try {
-                    System.out.println("Enter login: ");
-                    String login = scanner.nextLine();
-                    User user = new User(login);
-                    continue;
-                } catch (WrongLengthPasswordException e) {
-                    System.out.println("ERROR! " + e.getMessage());
-                }
-                attemptCounter++;
-            }
-            while (attemptCounter < COUNTER) {
-                try {
-                    System.out.println("Enter password: ");
-                    String password = scanner.nextLine();
-                    System.out.println("Enter confirm password: ");
-                    String confirmPassword = scanner.nextLine();
 
-                    User user = new User(password, confirmPassword);
+            try {
+                System.out.println("Enter login: ");
+                String login = scanner.nextLine();
+                System.out.println("Enter password: ");
+                String password = scanner.nextLine();
+                System.out.println("Enter confirm password: ");
+                String confirmPassword = scanner.nextLine();
 
-                    continue;
-                } catch (WrongLengthPasswordException e) {
-                    System.out.println("ERROR! " + e.getMessage());
-                } catch (WrongEqualsPasswordException e) {
-                    System.out.println("ERROR! " + e.getMessage());
-                } catch (WrongPasswordException e) {
-                    System.out.println("ERROR! " + e.getMessage());
-                }
+                User user = new User(login, password, confirmPassword);
+                System.out.printf("Your %s created successfully!\n", user.getLogin());
+                break;
+
+            } catch (WrongLoginException | WrongLengthPasswordException | WrongEqualsPasswordException |
+                     WrongPasswordException e) {
+                System.out.println("ERROR! " + e.getMessage());
 
                 attemptCounter++;
             }
-
-
-//                System.out.println("Enter login: ");
-//                String login = scanner.nextLine();
-//                System.out.println("Enter password: ");
-//                String password = scanner.nextLine();
-
-
-//                System.out.println("Enter confirm password: ");
-//                String confirmPassword = scanner.nextLine();
-//
-//                User user = new User(login, password, confirmPassword);
-//
-//                System.out.println("Confirm with registration! ");
-//                break;
-//
-//            } catch (WrongLoginException e) {
-//                System.out.println("ERROR! " + e.getMessage());
-//            } catch (WrongLengthPasswordException e) {
-//                System.out.println("ERROR! " + e.getMessage());
-//            } catch (WrongEqualsPasswordException e) {
-//                System.out.println("ERROR! " + e.getMessage());
-//            } catch (WrongPasswordException e) {
-//                System.out.println("ERROR! " + e.getMessage());
-//            }
-//            attemptCounter++;
-            if (attemptCounter > COUNTER - 1) {
+            if (attemptCounter >= COUNTER) {
                 System.out.println("No more attempts, please try again later !");
                 break;
             }
