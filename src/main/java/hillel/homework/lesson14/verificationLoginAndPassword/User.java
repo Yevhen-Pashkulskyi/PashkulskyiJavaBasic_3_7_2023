c3add6378f2790e63dbe2e0ec5f5c48ecdd25cbc
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2023-08-08 14:28:16 +0300

    lesson14 User login password version 1.0.7

--- src/main/java/hillel/homework/lesson14/verificationLoginAndPassword/User.java
+++ src/main/java/hillel/homework/lesson14/verificationLoginAndPassword/User.java
@@ -25,17 +25,17 @@ public class User {
         if (emptyAndNull(login)) {
             throw new WrongLoginException("Login cannot be null or empty");
         } else if (login.length() > MAX_LOGIN_LENGTH) {
-            throw new WrongLoginException(String.format("Login should be up to %d characters, and only English letters.", MAX_LOGIN_LENGTH));
+            throw new WrongLoginException(String.format("Login should be up to %d characters, and only English letters. ", MAX_LOGIN_LENGTH));
         } else if (!login.matches(REGEX_CONDITION_LOGIN)) {
-            throw new WrongLoginException("Only English letters.");
+            throw new WrongLoginException("Only English letters. ");
         }
     }
 
     private void validationPassword(String password, String confirmPassword) {
         if (emptyAndNull(password)) {
-            throw new WrongPasswordException("Password cannot be null or empty.");
+            throw new WrongPasswordException("Password cannot be null or empty. ");
         } else if (!password.matches(REGEX_CONDITION_PASSWORD)) {
-            throw new WrongPasswordException("The password must contain at least one letter, one number.");
+            throw new WrongPasswordException("The password must contain at least one letter, one number and not have symbol. ");
         } else if (!password.equals(confirmPassword)) {
             throw new WrongPasswordException("Password must match");
         } else if (MIN_PASSWORD_LENGTH > password.length() || password.length() > MAX_PASSWORD_LENGTH) { /*(!password.matches("^.{6,25}$")) { */
@@ -43,53 +43,9 @@ public class User {
         }
     }
 
-//    private void validationPassword(String password, String confirmPassword) {
-//        String errorLengthPassword = String.format("The length of the password must be between %d and %d characters.", MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH);
-//
-//        if (emptyAndNull(password)) {
-//            throw new WrongPasswordException("Password cannot be null or empty.");
-//        } else if (MIN_PASSWORD_LENGTH > password.length() || password.length() > MAX_PASSWORD_LENGTH) { /*(!password.matches("^.{6,25}$")) { */
-//            throw new WrongPasswordException("The length of the password must be between " + MIN_PASSWORD_LENGTH + " and " + MAX_PASSWORD_LENGTH + " characters.");
-//        } else if (!letterDigital(password) ) {
-//            throw new WrongPasswordException("The password must contain at least one letter and one number.");
-//        } else if (!password.equals(confirmPassword)) {
-//            throw new WrongPasswordException("Password must match");
-//        } else if (!password.matches(REGEX_CONDITION_LOGIN)) {
-//            throw new WrongPasswordException("Only letter and number.");
-//        }
-//    }
-
     public boolean emptyAndNull(String str) {
         return (str == null || str.equals(""));
     }
-
-//    public boolean letterDigital(String str) {
-//        char[] symbolPassword = str.toCharArray();
-//        boolean letter = false;
-//        boolean digit = false;
-//
-//        for (int i = 0; i < symbolPassword.length; i++) {
-//            if (Character.isLetter(symbolPassword[i])) {
-//                letter = true;
-//            } else if (Character.isDigit(symbolPassword[i])) {
-//                digit = true;
-//            }
-//            if (letter && digit) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-
-//    public int lengthPassword(String str) {
-//        int length = 0;
-//        char[] lengthPassword = str.toCharArray();
-//        for (int i = 0; i < lengthPassword.length; i++) {
-//            length++;
-//        }
-//        return length;
-
-    //    }
 
     public String getLogin() {
         return login;
